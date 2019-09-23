a6603b4379488575b123caa2b817b1a8aceb7b23
sandracubero <sandracuberor@gmail.com>
2019-09-23 20:45:14 +0200

    Create Docs

---
+++ docs/precache-manifest.ee209868fca36991fadee83eaa3e5245.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa8b7e6201d7c73b228ce7e6dbc89f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "273c2a6a670e29e024b7",
+    "url": "./static/css/main.db650daf.chunk.css"
+  },
+  {
+    "revision": "75844d0fe1a195404b81",
+    "url": "./static/js/2.704dd5e6.chunk.js"
+  },
+  {
+    "revision": "273c2a6a670e29e024b7",
+    "url": "./static/js/main.d3bd2110.chunk.js"
+  },
+  {
+    "revision": "fe461027164d6533c339",
+    "url": "./static/js/runtime-main.144315fd.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);
